73d69d0a0f7b75cca0449cebf0e9e12ef6a309b6
Qun Gu <qgu@chegg.com>
2024-06-06 13:22:58 -0700

    update

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+(setq package-check-signature nil)
 (require 'use-package)
 (setq use-package-always-ensure t)
 
